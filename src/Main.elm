347f423a9e4ba6ec9a5a921bb802941a65cab9a6
Adrian Vatchinsky <avatchinsky@dhcp-ccc-5435.redrover.cornell.edu>
2017-03-31 17:21:44 -0400

    made things move and added some helper functions

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,32 @@
 module Main exposing (..)
 
 import Task
-import Html.App
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Time exposing (Time)
 import Color exposing (Color)
 import Svg
 import Svg.Attributes exposing (..)
+import AnimationFrame
 import Window
 import String
 
 
-main : Program Never
+-- main : Program Never
+
+
 main =
-    Html.App.program
-        { init = ( initialModel, Task.perform (\_ -> NoOp) ResizeWindow Window.size )
+    Html.program
+        { init = ( initialModel, Cmd.none )
         , update = update
         , view = view
-        , subscriptions = (\_ -> Window.resizes ResizeWindow)
+        , subscriptions =
+            (\_ ->
+                Sub.batch
+                    [ Window.resizes ResizeWindow
+                    , AnimationFrame.times Tick
+                    ]
+            )
         }
 
 
@@ -39,6 +47,7 @@ initialModel =
             , radius = 100
             , color = Color.yellow
             , velocity = Vector 0 0
+            , acceleration = Vector 0 0
             }
         , Planet
             { name = "Planet Zebulon"
@@ -46,7 +55,8 @@ initialModel =
             , position = Coords 200 150
             , radius = 5
             , color = Color.red
-            , velocity = Vector 0 0
+            , velocity = Vector (0.25 * Basics.pi) 2
+            , acceleration = Vector 0 50
             }
         ]
     , money = 1000
@@ -62,6 +72,7 @@ type Body
         , radius : Float
         , color : Color
         , velocity : Vector
+        , acceleration : Vector --net acceleration
         }
 
 
@@ -98,7 +109,14 @@ update msg model =
             ( model, Cmd.none )
 
         Tick time ->
-            ( model, Cmd.none )
+            let
+                newBodies =
+                    List.map updatePosition model.bodies
+
+                newModel =
+                    { model | bodies = newBodies }
+            in
+                ( newModel, Cmd.none )
 
         ResizeWindow size ->
             ( { model | window = size }
@@ -106,20 +124,87 @@ update msg model =
             )
 
 
+updatePosition body =
+    case body of
+        Planet planet ->
+            let
+                newCoords =
+                    updateCoords planet.velocity planet.position
+            in
+                Planet { planet | position = newCoords }
+
+
+updateCoords (Vector angle speed) coords =
+    let
+        ( dx, dy ) =
+            fromPolar ( speed, angle )
+    in
+        { x = coords.x + dx
+        , y = coords.y + dy
+        }
+
+
+distance : Coords -> Coords -> Float
+distance p0 p1 =
+    sqrt <| (p1.x - p0.x) ^ 2 + (p1.y - p0.y) ^ 2
+
+
+angle : Coords -> Coords -> Float
+angle p0 p1 =
+    let
+        dx =
+            p1.x - p0.x
+
+        dy =
+            p1.y - p0.y
+
+        ( r, theta ) =
+            toPolar ( dx, dy )
+    in
+        theta
+
+
+g =
+    6.673 * 10 ^ -11
+
+
+
+-- N m2/kg2
+
+
+force : Body -> Body -> Vector
+force b0 b1 =
+    let
+        ( p0, m0 ) =
+            case b0 of
+                Planet planet ->
+                    ( planet.position, planet.mass )
+
+        ( p1, m1 ) =
+            case b1 of
+                Planet planet ->
+                    ( planet.position, planet.mass )
+
+        dist =
+            distance p0 p1
+
+        theta =
+            angle p0 p1
+
+        f_g =
+            (g * m0 * m1) / dist ^ 2
+    in
+        Vector theta f_g
+
+
 view : Model -> Html Msg
 view model =
     let
         left =
-            round
-                (-1
-                    * (toFloat model.window.width / 2.0)
-                )
+            round (-1 * (toFloat model.window.width / 2.0))
 
         top =
-            round
-                (-1
-                    * (toFloat model.window.height / 2.0)
-                )
+            round (-1 * (toFloat model.window.height / 2.0))
     in
         div
             [ Html.Attributes.style
@@ -149,15 +234,41 @@ viewBody : Body -> Html Msg
 viewBody body =
     case body of
         Planet planet ->
-            Svg.circle
-                [ r (toString planet.radius)
-                , cx (toString <| round planet.position.x)
-                , cy (toString <| round planet.position.y)
-                , Html.Attributes.style
-                    [ ( "fill", renderColor planet.color )
-                    ]
-                ]
+            Svg.g
                 []
+                [ Svg.circle
+                    [ r (toString planet.radius)
+                    , cx (toString <| round planet.position.x)
+                    , cy (toString <| round planet.position.y)
+                    , Html.Attributes.style
+                        [ ( "fill", renderColor planet.color )
+                        ]
+                    ]
+                    []
+                , viewVector planet.position planet.acceleration
+                ]
+
+
+viewVector : Coords -> Vector -> Html Msg
+viewVector pos (Vector angle scalar) =
+    let
+        ( dx, dy ) =
+            fromPolar ( scalar, angle )
+
+        targetX =
+            pos.x + dx
+
+        targetY =
+            pos.y + dy
+    in
+        Svg.line
+            [ x1 (toString pos.x)
+            , y1 (toString pos.y)
+            , x2 (toString targetX)
+            , y2 (toString targetY)
+            , stroke "pink"
+            ]
+            []
 
 
 renderColor : Color -> String
